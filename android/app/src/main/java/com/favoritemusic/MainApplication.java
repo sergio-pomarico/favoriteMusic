3fbd5b72ee60103a56812c7196eba44edc65486e
Sergio Pomarico <spomarico@reefstudios.co>
2018-12-25 08:32:49 -0500

    config fbsdk on android

--- android/app/src/main/java/com/favoritemusic/MainApplication.java
+++ android/app/src/main/java/com/favoritemusic/MainApplication.java
@@ -3,16 +3,27 @@ package com.favoritemusic;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -23,8 +34,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage()
+            new MainReactPackage(),
+            new VectorIconsPackage(),
+            new FBSDKPackage(mCallbackManager)
       );
     }
 
